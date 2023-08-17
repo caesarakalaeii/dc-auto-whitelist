7a9ea18e6d4810c08c748084fc2c3d9ac8b3e1ae
caesarakalaeii <caesarlp@gmx.de>
2023-08-17 16:23:11 +0200

    more tests

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ from config import *
 # Initialize bot and RCON client
 bot = commands.Bot(command_prefix="!")
 
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name} - {bot.user.id}")
